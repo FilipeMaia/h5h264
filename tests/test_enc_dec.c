7ca0d9e4cbe76b19ecca5a63977cda9c7ee95211
Filipe Maia <filipe.c.maia@gmail.com>
2016-02-17 17:43:52 +0100

    Show times in the test

--- tests/test_enc_dec.c
+++ tests/test_enc_dec.c
@@ -1,3 +1,4 @@
+#include <libavutil/time.h>
 #include "decode.c"
 #include "encode.c"
 
@@ -17,7 +18,7 @@ uint8_t * gen_data(int width, int height, int nframes){
 int main(){
   int width = 1000;
   int height = 1000;
-  int nframes = 123;
+  int nframes = 200;
   int size = width*height*nframes;
   char * data = (char *)gen_data(width, height, nframes);
   size_t encoded_size;
@@ -25,14 +26,24 @@ int main(){
   for(int i = 0;i<size;i++){
     data_sum += data[i];
   }
+  int64_t tic = av_gettime();
   char * encoded = h264_encode(data, size, height, width,
 			       1, &encoded_size);
+  int64_t toc = av_gettime();
+  float dt = (float)(toc-tic)/1.0e6;
+  printf("Encoded %d bytes in %f s - %g bytes/s\n", size, dt, size/dt);
 
   size_t decoded_size;
+  tic = av_gettime();
   char * decoded = h264_decode(encoded, encoded_size, &decoded_size);
-  for(int i = 0;i<size;i++){
-    if(data[i] != decoded[i]){
-      printf("Bytes %d differ! %d and %d\n", i, data[i], decoded[i]);
+  toc = av_gettime();
+  dt = (float)(toc-tic)/1.0e6;
+  printf("Decoded %d bytes in %f s - %g bytes/s\n", size, dt, size/dt);
+  if(memcmp(data, decoded, size)){
+    for(int i = 0;i<size;i++){
+      if(data[i] != decoded[i]){
+	printf("Bytes %d differ! %d and %d\n", i, data[i], decoded[i]);
+      }
     }
   }
   return 0;
